1966df120c76681517dc3599d309b0b9dd28855e
SapitoSucio <sapitosucio.mx@gmail.com>
2026-08-27 22:06:32 -0600

    fix(facebook): avoid invalid story filter branch labels

--- patches/src/main/kotlin/app/froggo/patches/facebook/ads/Facebook573StoryAdsPatch.kt
+++ patches/src/main/kotlin/app/froggo/patches/facebook/ads/Facebook573StoryAdsPatch.kt
@@ -4,7 +4,12 @@ import app.froggo.patches.shared.Constants.COMPATIBILITY_FACEBOOK_573
 import app.morphe.patcher.Fingerprint
 import app.morphe.patcher.extensions.InstructionExtensions.addInstructions
 import app.morphe.patcher.patch.bytecodePatch
+import app.morphe.patcher.util.proxy.mutableTypes.MutableMethod.Companion.toMutable
+import com.android.tools.smali.dexlib2.AccessFlags
 import com.android.tools.smali.dexlib2.Opcode
+import com.android.tools.smali.dexlib2.builder.MutableMethodImplementation
+import com.android.tools.smali.dexlib2.immutable.ImmutableMethod
+import com.android.tools.smali.dexlib2.immutable.ImmutableMethodParameter
 
 /*
  * Pre-release only: this file lives on the dev branch and is intentionally
@@ -49,6 +54,72 @@ val blockFacebookStoryAds573Patch = bytecodePatch(
     compatibleWith(COMPATIBILITY_FACEBOOK_573)
 
     execute {
+        val targetClass = storyBucketProcessing.classDef
+        val targetClassType = targetClass.type
+        val filterMethodName = "froggoFilterStoryAds"
+
+        val filterMethod = ImmutableMethod(
+            targetClassType,
+            filterMethodName,
+            listOf(
+                ImmutableMethodParameter(
+                    "Lcom/google/common/collect/ImmutableList;",
+                    null,
+                    null,
+                ),
+            ),
+            "Lcom/google/common/collect/ImmutableList;",
+            AccessFlags.PRIVATE.value or AccessFlags.STATIC.value,
+            null,
+            null,
+            MutableMethodImplementation(5),
+        ).toMutable().apply {
+            // This helper is a whole new method whose instructions start at dex
+            // offset zero. Keeping all loop labels here avoids non-zero-index
+            // label rebasing issues in addInstructions(...).
+            addInstructions(
+                0,
+                """
+                    invoke-interface {p0}, Ljava/util/List;->iterator()Ljava/util/Iterator;
+                    move-result-object v0
+
+                    :froggo_storyads_scan_loop
+                    invoke-interface {v0}, Ljava/util/Iterator;->hasNext()Z
+                    move-result v1
+                    if-eqz v1, :froggo_storyads_filter_exit
+                    invoke-interface {v0}, Ljava/util/Iterator;->next()Ljava/lang/Object;
+                    move-result-object v1
+                    instance-of v2, v1, LX/9XO;
+                    if-eqz v2, :froggo_storyads_scan_loop
+
+                    new-instance v0, Ljava/util/ArrayList;
+                    invoke-direct {v0}, Ljava/util/ArrayList;-><init>()V
+                    invoke-interface {p0}, Ljava/util/List;->iterator()Ljava/util/Iterator;
+                    move-result-object v1
+
+                    :froggo_storyads_filter_loop
+                    invoke-interface {v1}, Ljava/util/Iterator;->hasNext()Z
+                    move-result v2
+                    if-eqz v2, :froggo_storyads_filter_done
+                    invoke-interface {v1}, Ljava/util/Iterator;->next()Ljava/lang/Object;
+                    move-result-object v2
+                    instance-of v3, v2, LX/9XO;
+                    if-nez v3, :froggo_storyads_filter_loop
+                    invoke-virtual {v0, v2}, Ljava/util/ArrayList;->add(Ljava/lang/Object;)Z
+                    goto :froggo_storyads_filter_loop
+
+                    :froggo_storyads_filter_done
+                    invoke-static {v0}, Lcom/google/common/collect/ImmutableList;->copyOf(Ljava/util/Collection;)Lcom/google/common/collect/ImmutableList;
+                    move-result-object p0
+
+                    :froggo_storyads_filter_exit
+                    return-object p0
+                """.trimIndent(),
+            )
+        }
+
+        targetClass.methods.add(filterMethod)
+
         val returnIndex = storyBucketProcessing.method.implementation!!.instructions
             .withIndex()
             .last { (_, instruction) -> instruction.opcode == Opcode.RETURN_OBJECT }
@@ -57,40 +128,8 @@ val blockFacebookStoryAds573Patch = bytecodePatch(
         storyBucketProcessing.method.addInstructions(
             returnIndex,
             """
-                invoke-interface {p2}, Ljava/util/List;->iterator()Ljava/util/Iterator;
-                move-result-object v0
-
-                :froggo_storyads_scan_loop
-                invoke-interface {v0}, Ljava/util/Iterator;->hasNext()Z
-                move-result v1
-                if-eqz v1, :froggo_storyads_filter_exit
-                invoke-interface {v0}, Ljava/util/Iterator;->next()Ljava/lang/Object;
-                move-result-object v1
-                instance-of v2, v1, LX/9XO;
-                if-eqz v2, :froggo_storyads_scan_loop
-
-                new-instance v0, Ljava/util/ArrayList;
-                invoke-direct {v0}, Ljava/util/ArrayList;-><init>()V
-                invoke-interface {p2}, Ljava/util/List;->iterator()Ljava/util/Iterator;
-                move-result-object v1
-
-                :froggo_storyads_filter_loop
-                invoke-interface {v1}, Ljava/util/Iterator;->hasNext()Z
-                move-result v2
-                if-eqz v2, :froggo_storyads_filter_done
-                invoke-interface {v1}, Ljava/util/Iterator;->next()Ljava/lang/Object;
-                move-result-object v2
-                instance-of v3, v2, LX/9XO;
-                if-nez v3, :froggo_storyads_filter_loop
-                invoke-virtual {v0, v2}, Ljava/util/ArrayList;->add(Ljava/lang/Object;)Z
-                goto :froggo_storyads_filter_loop
-
-                :froggo_storyads_filter_done
-                invoke-static {v0}, Lcom/google/common/collect/ImmutableList;->copyOf(Ljava/util/Collection;)Lcom/google/common/collect/ImmutableList;
-                move-result-object v0
-                move-object p2, v0
-
-                :froggo_storyads_filter_exit
+                invoke-static {p2}, $targetClassType->$filterMethodName(Lcom/google/common/collect/ImmutableList;)Lcom/google/common/collect/ImmutableList;
+                move-result-object p2
             """.trimIndent(),
         )
     }
